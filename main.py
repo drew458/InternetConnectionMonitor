56a648572192675f93eab7c5271f83e27afe03d5
drew458 <43808240+drew458@users.noreply.github.com>
2020-12-09 11:25:25 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+import time
+import requests
+
+
+def executeSomething():
+    url = "http://www.google.com"
+    timeout = 5
+    try:
+        request = requests.get(url, timeout=timeout)
+        print("Connected to the Internet")
+    except (requests.ConnectionError, requests.Timeout) as exception:
+        print("No internet connection.")
+    time.sleep(5)
+
+
+while True:
+    executeSomething()
